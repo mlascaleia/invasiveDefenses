e6d2a65abedcaf403f1321987a333585b4dd2a42
Olivia Thorgersen <oliviathorgersen@dhcp-10-250-35-10.harvard.edu>
2025-08-06 13:46:09 -0400

    Editing graphs for final presentation

--- olivia/datavisualizationolivia.R
+++ olivia/datavisualizationolivia.R
@@ -1,39 +1,56 @@
 # the purpose of this script is to create box plots of all the data
 # and save them as png's in the folder "Plots"
 
+
+
+
+
 # clear environment
 rm(list = ls())
-
 
 #load packages
 library(tidyverse)
 library(ggplot2)
 library(ggthemes)
 
-
-
 # Define your plotting function
 create_boxplot <- function(data, y_var, y_label, file_suffix) {
+  # Convert treatment to factor with specified order
+  data$treatment <- factor(data$treatment, levels = c("LP", "LA", "AP", "AA"))
+  
   ggplot(data = data, mapping = aes(x = treatment, y = {{ y_var }})) +
-    geom_boxplot(aes(fill = treatment)) +
-    geom_point() +
-    scale_fill_manual(values = c("lightsalmon","peachpuff","darkolivegreen4","darkolivegreen2"), 
-                      name = "Treatments") +
-    scale_x_discrete(label=c("NR","EI","ER","EA"))+
+    geom_boxplot(aes(fill = treatment),
+                 color = "white",               # Outline color
+                 outlier.color = "white",       # Outlier points color
+                 alpha = 0.7) +                 # Slightly transparent fill
+    stat_boxplot(geom = "errorbar",             # Whiskers
+                 color = "white",
+                 width = 0.2) +
+    geom_point(color = "white") +
+    scale_fill_manual(values = c("darkolivegreen4", "olivedrab3", "lightsalmon", "peachpuff"), 
+                      name = "Treatments",
+                      labels = c("Native Intact", "Native Removed", "Exotic Intact", "Exotic Removed")) +
     xlab("Treatments") +
     ylab(y_label) +
     theme_tufte() +
-    theme(legend.position = "none",
-          plot.title = element_text(hjust = 0.5))
+    theme(
+      legend.position = "none",
+      text = element_text(color = "white"),
+      axis.text = element_text(color = "white"),
+      axis.text.x = element_blank(),
+      axis.title = element_text(color = "white"),
+      axis.ticks = element_line(color = "white"),
+      axis.ticks.x = element_blank(),
+      legend.text = element_text(color = "white"),
+      legend.title = element_text(color = "white"),
+      plot.title = element_text(hjust = 0.5))
   
   # Save with dynamic filename
   ggsave(paste0("olivia/finalplots/", file_suffix, ".png"))
 }
 
-
 # Function to process all specified columns
-process_columns <- function(data, columns_to_plot)
-  
+process_columns <- function(data, columns_to_plot) {
   # Loop through each column specification
   for (col_spec in columns_to_plot) {
     col_name <- col_spec$column
@@ -46,6 +63,7 @@ process_columns <- function(data, columns_to_plot)
                    y_label = y_label, 
                    file_suffix = file_suffix)
   }
+}
 
 #chemical analyses
 all_data <- read_csv("olivia/Harvard Master2.csv")
@@ -59,16 +77,13 @@ columns_to_plot <- list(
   list(column = "Saponins", label = "Saponins", file_suffix = "Saponins"),
   list(column = "Terpenoids", label = "Terpenoids", file_suffix = "Terpenoids"),
   list(column = "Tannins", label = "Tannins", file_suffix = "Tannins")
-  # Add more columns as needed
 )
 
 # Run the plotting process
 process_columns(all_data, columns_to_plot)
 
-
-
 #physical tests
-all_data_physical <- read_csv("olivia/physicalData.csv") #rename based on file name
+all_data_physical <- read_csv("olivia/physicalData.csv")
 all_data_physical$treatment <- stringr::str_extract(all_data_physical$"Leaf Number", "^..")
 colnames(all_data_physical)
 
@@ -84,4 +99,95 @@ columns_to_plot_physical <- list(
 # Run the plotting process
 process_columns(all_data_physical, columns_to_plot_physical)
 
+
+
+
+# # clear environment
+# rm(list = ls())
+# 
+# 
+# #load packages
+# library(tidyverse)
+# library(ggplot2)
+# library(ggthemes)
+# 
+# 
+# 
+# # Define your plotting function
+# create_boxplot <- function(data, y_var, y_label, file_suffix) {
+#   ggplot(data = data, mapping = aes(x = treatment, y = {{ y_var }})) +
+#     geom_boxplot(aes(fill = treatment)) +
+#     geom_point() +
+#     scale_fill_manual(values = c("darkolivegreen4","olivedrab3", "lightsalmon","peachpuff"), 
+#                       name = "Treatments") +
+#     xlab("Treatments") +
+#     ylab(y_label) +
+#     theme_tufte() +
+#     theme(
+#       legend.position = "none",
+#       text = element_text(color = "white"),  # All text
+#       axis.text = element_text(color = "white"),  # Axis numbers
+#       axis.title = element_text(color = "white"),  # Axis labels
+#       axis.ticks = element_line(color = "white"),  # Axis ticks
+#       legend.text = element_text(color = "white"),  # Legend text
+#       legend.title = element_text(color = "white"),
+#       plot.title = element_text(hjust = 0.5))
+#   
+#   # Save with dynamic filename
+#   ggsave(paste0("olivia/finalplots/", file_suffix, ".png"))
+# }
+# 
+# 
+# # Function to process all specified columns
+# process_columns <- function(data, columns_to_plot)
+#   
+#   # Loop through each column specification
+#   for (col_spec in columns_to_plot) {
+#     col_name <- col_spec$column
+#     y_label <- col_spec$label
+#     file_suffix <- col_spec$file_suffix
+#     
+#     # Create the plot
+#     create_boxplot(data = data, 
+#                    y_var = !!sym(col_name), 
+#                    y_label = y_label, 
+#                    file_suffix = file_suffix)
+#   }
+# 
+# #chemical analyses
+# all_data <- read_csv("olivia/Harvard Master2.csv")
+# all_data$treatment <- stringr::str_extract(all_data$Leaf.Number, "^..")
+# colnames(all_data)
+# 
+# # Define which columns to plot and their labels
+# columns_to_plot <- list(
+#   list(column = "Flavonoids", label = "Flavonoids", file_suffix = "Flavonoids"),
+#   list(column = "Phenolics", label = "Phenolics", file_suffix = "Phenolics"),
+#   list(column = "Saponins", label = "Saponins", file_suffix = "Saponins"),
+#   list(column = "Terpenoids", label = "Terpenoids", file_suffix = "Terpenoids"),
+#   list(column = "Tannins", label = "Tannins", file_suffix = "Tannins")
+#   # Add more columns as needed
+# )
+# 
+# # Run the plotting process
+# process_columns(all_data, columns_to_plot)
+# 
+# 
+# 
+# #physical tests
+# all_data_physical <- read_csv("olivia/physicalData.csv") #rename based on file name
+# all_data_physical$treatment <- stringr::str_extract(all_data_physical$"Leaf Number", "^..")
+# colnames(all_data_physical)
+# 
+# # Define which columns to plot and their labels
+# columns_to_plot_physical <- list(
+#   list(column = "Toughness (N)", label = "Toughness (N)", file_suffix = "Toughness"),
+#   list(column = "Thickness (mm)", label = "Thickness (mm)", file_suffix = "Thickness"),
+#   list(column = "Mass (mg)", label = "Mass (mg)", file_suffix = "Mass"),
+#   list(column = "Dried Mass (mg)", label = "Dry Mass (mg)", file_suffix = "DryMass"),
+#   list(column = "Caterpillar Mass Change", label = "Change in Mass (mg)", file_suffix = "CaterpillarMass")
+# )
+# 
+# # Run the plotting process
+# process_columns(all_data_physical, columns_to_plot_physical)
 
--- olivia/herbivoryTimeseries.R
+++ olivia/herbivoryTimeseries.R
@@ -15,57 +15,35 @@ df <- read.csv("olivia/Scans.csv") %>%
   mutate(time = as.integer(time))
 
 
-# add in dummy data
-# (things should usually go down but they may go up a little with the dummy data)
+library(tidyverse)
+library(ggplot2)
+library(ggthemes)
 
+df <- read.csv("olivia/Scans.csv") %>%
+  rename(`24` = Damage.at.24.HR....,
+         `48` = Damage.at.48.HR....,
+         `72` = Damage.at.72.HR....,
+         `96` = Damage.at.96.HR....) %>%
+  mutate(`0` = 100,
+         treatment = str_extract(Leaf.Number, "^..")) %>%
+  select(id = Leaf.Number, treatment, `0`, `24`, `48`, `72`, `96`) %>%
+  pivot_longer(`0`:`96`, names_to = "time", values_to = "damage") %>%
+  mutate(time = as.integer(time))
+
+# Convert treatment to a factor with the desired order
+df$treatment <- factor(df$treatment, levels = c("LP", "LA", "AP", "AA"))
+
+# add in dummy data
 df$damage[is.na(df$damage)] <- 100 - (rlnorm(length(df$damage[is.na(df$damage)]), 0, 0.5 ) *
                                         df$time[is.na(df$damage)]/6)
 
 # summarize
-
 df.sum <- df %>%
   group_by(treatment, time) %>%
   summarise(meanDamage = mean(damage), se = sd(damage)/sqrt(6)) %>%
   mutate(semax = meanDamage + (1.96*se),
          semin = meanDamage - (1.96*se))
 
-
-# make figure
-ggplot(data = df.sum, aes(x = time, y = meanDamage)) +
-  geom_ribbon(aes(ymin = semin, ymax = semax, fill = treatment),
-              alpha = .2) +
-  geom_line(aes(color = treatment), linewidth = 2) +
-  geom_line(data = df, aes(y = damage, group = id, color = treatment),
-            linewidth = 0.5, alpha = 0.5) +
-  xlab("Time (Hours)")+
-  ylab("Leaf Damage (%)")+
-  scale_x_continuous(
-    name = "Time (Hours)",
-    breaks = seq(0, 96, by = 24),
-    limits = c(0, 96),
-    labels = scales::comma
-  ) +
-  facet_wrap(
-    ~treatment, 
-    nrow = 2,
-    labeller = labeller(treatment = c(
-      "AA" = "EA",      # Replace with your actual treatment names
-      "AP" = "EP",        # and desired labels
-      "LA" = "NA",
-      "LP" = "NP"
-    ))
-  ) +
-  theme_tufte(18) +
-  theme(legend.position = "none")+
-  scale_fill_manual(values = c("lightsalmon","peachpuff","darkolivegreen4","darkolivegreen3"))+
-  scale_color_manual(values = c("lightsalmon","peachpuff","darkolivegreen4","darkolivegreen3"))
-  
-
-
-ggsave("olivia/totalLeafDamage.png") #rename and save image
-
-
-
 ggplot(data = df.sum, aes(x = time, y = meanDamage)) +
   geom_ribbon(aes(ymin = semin, ymax = semax, fill = treatment),
               alpha = .2) +
@@ -79,13 +57,61 @@ ggplot(data = df.sum, aes(x = time, y = meanDamage)) +
     labels = scales::comma
   ) +
   theme_tufte(18) +
-  theme(legend.position = "none")+
-  scale_fill_manual(values = c("lightsalmon","peachpuff","darkolivegreen4","darkolivegreen3"))+
-  scale_color_manual(values = c("lightsalmon","peachpuff","darkolivegreen4","darkolivegreen3"))+
-  labs(fill = "Treatment", color = "Treatment")  # Changes the legend title
-
+  theme(
+    legend.position = "none",
+    text = element_text(color = "white"),  # All text
+    axis.text = element_text(color = "white"),  # Axis numbers
+    axis.title = element_text(color = "white"),  # Axis labels
+    axis.ticks = element_line(color = "white"),  # Axis ticks
+    panel.grid.major = element_line(color = "white", size = 0.1),
+    panel.grid.minor = element_blank(),
+    legend.text = element_text(color = "white"),  # Legend text
+    legend.title = element_text(color = "white"))+
+  scale_fill_manual(values = c("darkolivegreen4","olivedrab3", "lightsalmon","peachpuff"), labels = c("Native Intact", "Native Removed", "Exotic Intact", "Exotic Removed"))+
+  scale_color_manual(values = c("darkolivegreen4","olivedrab3", "lightsalmon","peachpuff"), labels = c("Native Intact", "Native Removed", "Exotic Intact", "Exotic Removed"))+
+  labs(fill = "Treatment", color = "Treatment")
 
 ggsave("olivia/meanDamage.png")
+
+
+
+
+
+#EXTRA GRAPH
+
+# # make figure
+# ggplot(data = df.sum, aes(x = time, y = meanDamage)) +
+#   geom_ribbon(aes(ymin = semin, ymax = semax, fill = treatment),
+#               alpha = .2) +
+#   geom_line(aes(color = treatment), linewidth = 2) +
+#   geom_line(data = df, aes(y = damage, group = id, color = treatment),
+#             linewidth = 0.5, alpha = 0.5) +
+#   xlab("Time (Hours)")+
+#   ylab("Leaf Damage (%)")+
+#   scale_x_continuous(
+#     name = "Time (Hours)",
+#     breaks = seq(0, 96, by = 24),
+#     limits = c(0, 96),
+#     labels = scales::comma
+#   ) +
+#   facet_wrap(
+#     ~treatment, 
+#     nrow = 2,
+#     labeller = labeller(treatment = c(
+#       "AA" = "EA",      # Replace with your actual treatment names
+#       "AP" = "EP",        # and desired labels
+#       "LA" = "NA",
+#       "LP" = "NP"
+#     ))
+#   ) +
+#   theme_tufte(18) +
+#   theme(legend.position = "none")+
+#   scale_fill_manual(values = c("lightsalmon","peachpuff","darkolivegreen4","darkolivegreen3"))+
+#   scale_color_manual(values = c("lightsalmon","peachpuff","darkolivegreen4","darkolivegreen3"))
+# 
+# 
+# 
+# ggsave("olivia/totalLeafDamage.png") #rename and save image
 
 
 
