72f24e555d456ddfb86862f8ef3f18fc666ea36c
ishaisha <ishaisha@umich.edu>
2026-07-22 10:51:38 -0400

    cleaning up nmds and t-test code

--- isha/NMDS.R
+++ isha/NMDS.R
@@ -1,5 +1,6 @@
-# the purpose of this script is to run nmds and pca for all my data
-# and create pngs of the data
+# the purpose of this script is to compare different groupings for this study
+# and run an nmds and permanova for the best model
+# and then create pngs of the plots
 
 # clear environment
 rm(list = ls())
@@ -10,6 +11,7 @@ library(dplyr)
 library(vegan)
 library(ggplot2)
 library(ggfortify)
+
 
 master <- read.csv("isha/Harvard Master.csv")
 
@@ -34,97 +36,38 @@ selected_data <- my_data %>%
   )
 grouping_var <- as.factor(my_data$Type)
 
-# run NMDS
-nmds_result <- metaMDS(selected_data, 
-                       distance = "bray",  # or "jaccard" for presence/absence
-                       k = 2,             # number of dimensions
-                       try = 20,          # number of random starts
-                       trymax = 100,       # maximum number of iterations
-                       autotransform = TRUE)  # automatic data transformation
 
-# stress value
-nmds_result$stress
+# comparing nmds groupings ----
+grouping_var <- my_data$Type
 
-# michael trying a few other NMDS things
+groupings <- list(
+  "All 3 groups" = grouping_var,
+  "Invasive vs (NIE+Native)" = ifelse(grouping_var == "Invasive", "Invasive", "Non-invasive"),
+  "NIE vs (Native+Invasive)" = ifelse(grouping_var == "Non-invasive exotic", "NIE", "Others"),
+  "Native vs (Invasive+NIE)" = ifelse(grouping_var == "Native", "Native", "Others"))
 
-# run NMDS
-nmds_result2 <- metaMDS(selected_data, 
-                       distance = "bray",  # or "jaccard" for presence/absence
-                       k = 3,             # number of dimensions
-                       try = 20,          # number of random starts
-                       trymax = 100,       # maximum number of iterations
-                       autotransform = TRUE)  # automatic data transformation
+results <- data.frame()
 
-# stress value
-nmds_result2$stress
+for (i in 1:length(groupings)) {
+  
+  group_name <- names(groupings)[i]
+  group_vector <- groupings[[i]]
+  
+  # Run PERMANOVA
+  result <- adonis2(selected_data ~ group_vector, method = "bray")
+  
+  # Store results
+  results <- rbind(results, data.frame(
+    Grouping = group_name,
+    R2 = result$R2[1],
+    P_value = result$`Pr(>F)`[1]
+  ))
+}
 
-
-# make plot ####
-
-env_vectors <- envfit(nmds_result, selected_data, permutations = 999, na.rm = TRUE)
-
-
-my_point_colors <- c("thistle3", "olivedrab4", "gold")
-my_ellipse_colors <- c("thistle", "olivedrab3", "lemonchiffon")
-my_point_shapes <- c(16, 17, 15)
-
-png("isha/Plots/NMDS_plot.png", 
-    width = 10, 
-    height = 6, 
-    units = "in", 
-    res = 300,
-    bg = "transparent")  
-
-par(family = "serif", bg = NA)
-plot(nmds_result, type = "n", main = "NMDS Plot of Species Type Data", bg = NA)
+results
 
 
-points(nmds_result, display = "sites", 
-       pch = my_point_shapes[as.numeric(grouping_var)],
-       col = my_point_colors[as.numeric(grouping_var)],
-       cex = 1.5)
-
-# add ellipses
-ordiellipse(
-  nmds_result,
-  groups = grouping_var,
-  kind = "sd",
-  draw = "polygon",
-  col = my_ellipse_colors,
-  border = "black",       
-  lwd = 1,              
-  alpha = 100             
-)
-
-# add environmental vectors
-plot(env_vectors, col = "black", lwd = 2, cex = 0.7)
-
-
-# add legend
-legend(x = "topright",
-       legend = levels(grouping_var),
-       pch = my_point_shapes[1:length(levels(grouping_var))],
-       col = my_point_colors[1:length(levels(grouping_var))],
-       pt.bg = my_point_colors[1:length(levels(grouping_var))],
-       title = "Species Type",
-       xpd = TRUE
-)
-
-
-dev.off()
-
-
-
-# two group nmds attempt by isha --------
-
-# clear environment
-rm(list = ls())
-
-# load packages
-library(dplyr)
-library(vegan)
-library(ggplot2)
-library(ggfortify)
+# nmds of invasive vs non-invasive --------
 
 master <- read.csv("isha/Harvard Master.csv")
 
@@ -155,36 +98,9 @@ selected_data <- my_data %>%
     SpecificLeafArea = SLA..mm2.mg.
   )
 
-# re-pasting grouped nmds attempt
-grouping_var <- my_data$Type
 
-groupings <- list(
-  "All 3 groups" = grouping_var,
-  "Invasive vs (NIE+Native)" = ifelse(grouping_var == "Invasive", "Invasive", "Non-invasive"),
-  "NIE vs (Native+Invasive)" = ifelse(grouping_var == "Non-invasive exotic", "NIE", "Others"),
-  "Native vs (Invasive+NIE)" = ifelse(grouping_var == "Native", "Native", "Others"))
 
-results <- data.frame()
-
-for (i in 1:length(groupings)) {
-  
-  group_name <- names(groupings)[i]
-  group_vector <- groupings[[i]]
-  
-  # Run PERMANOVA
-  result <- adonis2(selected_data ~ group_vector, method = "bray")
-  
-  # Store results
-  results <- rbind(results, data.frame(
-    Grouping = group_name,
-    R2 = result$R2[1],
-    P_value = result$`Pr(>F)`[1]
-  ))
-}
-
-results
-
-# run NMDS
+# run NMDS ----
 nmds_result <- metaMDS(selected_data, 
                        distance = "bray",  # or "jaccard" for presence/absence
                        k = 2,             # number of dimensions
@@ -195,27 +111,17 @@ nmds_result <- metaMDS(selected_data,
 # stress value
 nmds_result$stress
 
-# Try with k=3
-nmds_result2 <- metaMDS(selected_data, 
-                        distance = "bray",  # or "jaccard" for presence/absence
-                        k = 3,             # number of dimensions
-                        try = 20,          # number of random starts
-                        trymax = 100,       # maximum number of iterations
-                        autotransform = TRUE)  # automatic data transformation
 
-# stress value
-nmds_result2$stress
-
-# make plot ####
+# make plot ----
 env_vectors <- envfit(nmds_result, selected_data, permutations = 999, na.rm = TRUE)
 
 # Update colors and shapes for 2 groups
-my_point_colors <- c("olivedrab4", "gold")  # Invasive, Non-invasive
-my_ellipse_colors <- c("olivedrab3", "lemonchiffon")  # Invasive, Non-invasive
+my_point_colors <- c("plum", "olivedrab4")  # Invasive, Non-invasive
+my_ellipse_colors <- c("thistle", "olivedrab3")  # Invasive, Non-invasive
 my_point_shapes <- c(16, 17)  # Different shapes for each group
 
-png("isha/Plots/NMDS_plot_2groups.png", 
-    width = 10, 
+png("isha/Plots/NMDS.png", 
+    width = 8, 
     height = 6, 
     units = "in", 
     res = 300,
@@ -256,143 +162,9 @@ legend(x = "topright",
 
 dev.off()
 
-# Optional: Add statistical test to see if groups are significantly different
-# Permutation test for differences between groups
+# permanova ----
 adonis_result <- adonis2(selected_data ~ Group, data = my_data, method = "bray", permutations = 999)
 print(adonis_result)
-
-
-
-
-
-
-
-----------------------------------------------------------------------
-
-  # # try PCA ####
-# 
-# # drop categorical variable(s)
-# pca_data <- selected_data %>%
-#   select(-GrowthForm)
-# 
-# # run PCA on scaled correlation matrix
-# pca_result <- prcomp(pca_data, center = TRUE, scale = TRUE)
-# summary(pca_result)
-# screeplot(pca_result, type = "lines", main = "Scree Plot")
-# 
-# 
-# # put back in species info
-# pca_result
-# 
-# # loadings (variable contributions to each PC)
-# pca_result$rotation
-# 
-# # scores (sample positions in PC space)
-# pca_result$x
-# pca_scores <- as.data.frame(pca_result$x)
-# pca_scores$Species.Name <- my_data$Species.Name
-# pca_scores$Type <- grouping_var
-# 
-# # make pca plot ####
-# 
-# # extract loadings for biplot arrows
-# pca_loadings <- as.data.frame(pca_result$rotation[, 1:2])
-# pca_loadings$Variable <- rownames(pca_loadings)
-# 
-# # scale loadings for visual clarity (adjust multiplier as needed)
-# loading_scale <- 3
-# 
-# # variance explained for axis labels
-# var_explained <- summary(pca_result)$importance[2, 1:2] * 100
-# 
-# ggplot() +
-#   # sample points, colored by Type
-#   geom_point(data = pca_scores,
-#              aes(x = PC1, y = PC2, color = Type),
-#              size = 3, alpha = 0.8) +
-#   # species name labels
-#   geom_text(data = pca_scores,
-#             aes(x = PC1, y = PC2, label = Species.Name, color = Type),
-#             size = 2.8, vjust = -0.8, hjust = 0.5, show.legend = FALSE) +
-#   # loading arrows
-#   geom_segment(data = pca_loadings,
-#                aes(x = 0, y = 0,
-#                    xend = PC1 * loading_scale,
-#                    yend = PC2 * loading_scale),
-#                arrow = arrow(length = unit(0.2, "cm"), type = "closed"),
-#                color = "gray40", linewidth = 0.5, alpha = 0.7) +
-#   # loading labels
-#   geom_text(data = pca_loadings,
-#             aes(x = PC1 * loading_scale * 1.15,
-#                 y = PC2 * loading_scale * 1.15,
-#                 label = Variable),
-#             size = 2.5, color = "gray30") +
-#   # axis labels with variance explained
-#   labs(x = paste0("PC1 (", round(var_explained[1], 1), "%)"),
-#        y = paste0("PC2 (", round(var_explained[2], 1), "%)"),
-#        color = "Type") +
-#   # origin crosshairs
-#   geom_hline(yintercept = 0, linetype = "dashed", color = "gray70", linewidth = 0.4) +
-#   geom_vline(xintercept = 0, linetype = "dashed", color = "gray70", linewidth = 0.4) +
-#   theme_classic() +
-#   theme(legend.position = "right",
-#         axis.title = element_text(size = 11),
-#         axis.text = element_text(size = 9),
-#         legend.title = element_text(size = 10),
-#         legend.text = element_text(size = 9))
-# 
-  
-  
-# # PCA Analysis ----
-# 
-# # 1. Scale the data (important for PCA with different measurement units)
-# scaled_data <- scale(selected_data)
-# 
-# # 2. Run PCA
-# pca_result <- prcomp(scaled_data)
-# 
-# # 3. View summary
-# summary(pca_result)
-# 
-# # 4. View loadings (variable contributions)
-# print(pca_result$rotation)
-# 
-# # 5. View eigenvalues (variance explained)
-# pca_eigenvalues <- pca_result$sdev^2
-# print(pca_eigenvalues)
-# 
-# 
-# 
-# # PCA Visualization ----
-# 
-# # Scree plot (variance explained by each PC)
-# png("isha/Plots/pca_scree_plot.png", width = 8, height = 6, units = "in", res = 300)
-# plot(pca_eigenvalues, type = "b", pch = 19, 
-#      xlab = "Principal Component", ylab = "Eigenvalue",
-#      main = "Scree Plot of PCA")
-# dev.off()
-# 
-# # Biplot with groups
-# png("isha/Plots/pca_biplot.png", width = 8, height = 6, units = "in", res = 300)
-# biplot(pca_result, cex = c(0.7, 0.9), 
-#        col = c("gray50", "black"),
-#        xlab = paste0("PC1 (", round(summary(pca_result)$importance[2,1]*100, 1), "%)"),
-#        ylab = paste0("PC2 (", round(summary(pca_result)$importance[2,2]*100, 1), "%)"))
-# dev.off()
-# 
-# # Enhanced ggplot version with ellipses
-# png("isha/Plots/pca_ggplot.png", width = 8, height = 6, units = "in", res = 300)
-# autoplot(pca_result, data = my_data, colour = 'Type',
-#          loadings = TRUE, loadings.label = TRUE,
-#          loadings.label.size = 3, loadings.label.colour = 'black',
-#          frame = TRUE, frame.type = 'norm') +
-#   scale_color_manual(values = my_point_colors) +
-#   theme_minimal() +
-#   ggtitle("PCA of Plant Traits by Species Type")
-# dev.off()
-
-
-
 
 
 
--- isha/T-Tests/tTest_dataVisualization.R
+++ isha/T-Tests/tTest_dataVisualization.R
@@ -9,6 +9,8 @@ rm(list = ls())
 library(dplyr)
 library(ggplot2)
 
+
+
 # Read data
 plant_data <- read.csv("isha/Harvard Master.csv", header = TRUE)
 
@@ -23,7 +25,7 @@ traits <- c("Plant.height..ft.", "Nitrogen.content", "C.N.ratio",
 # Create empty dataframe for results
 results <- data.frame()
 
-# Run t-tests
+# Whole plant traits t-tests ----
 for (trait in traits) {
   # T-test using binary variable
   test <- t.test(plant_data[[trait]] ~ plant_data$isInvasive)
@@ -50,10 +52,9 @@ print(results)
 # Save results
 write.csv(results, "isha/T-Tests/t_test_whole_results.csv", row.names = FALSE)
 
-# Show only significant results
-print(results[results$Significant == "Yes", ])
 
-# Leaf traits ----
+
+# Leaf traits t-tests ----
 
 leaf_data <- read.csv("isha/Harvard MasterLeaf.csv", header = TRUE)
 
@@ -101,8 +102,7 @@ print(results)
 # Save results (directly in isha folder, not in subfolder)
 write.csv(results, "isha/T-Tests/t_test_leaf_results.csv", row.names = FALSE)
 
-# Show only significant results
-print(results[results$Significant == "Yes", ])
+
 
 
 # Data visualization ----
@@ -143,192 +143,6 @@ for (trait in leaf_traits) {
   
   ggsave(paste0("isha/Plots/", trait, ".png"), p)
 }
-
-
-
-
-
-
-
-
-
-# whole_tukey <- read_csv("isha/ANOVA/whole_plant_tukey_results.csv")
-# leaf_tukey <- read_csv("isha/ANOVA/leaf_tukey_results.csv")
-# 
-# whole_tukey <- whole_tukey %>%
-#   mutate(Variable = case_when(
-#     Variable == "SLA..mm2.mg." ~ "SLA (mm2/mg)",
-#     Variable == "Average.water.content...." ~ "Average water content (%)",
-#     Variable == "Nitrogen.content" ~ "Nitrogen content",
-#     Variable == "C.N.ratio" ~ "C:N ratio",
-#     Variable == "Plant.height..ft." ~ "Plant height (ft)",
-#     TRUE ~ Variable
-#   ))
-# 
-# leaf_tukey <- leaf_tukey %>%
-#   mutate(Variable = case_when(
-#     Variable == "Toughness..N." ~ "Toughness (N)",
-#     Variable == "Thickness..mm." ~ "Thickness (mm)",
-#     TRUE ~ Variable
-#   ))
-# 
-# all_tukey <- bind_rows(
-#   whole_tukey %>% mutate(Source = "Whole"),
-#   leaf_tukey %>% mutate(Source = "Leaf")
-# )
-# 
-# create_boxplot <- function(data, y_var, y_label, file_suffix, tukey_data, plot_label = NULL) {
-#   y_var_str <- as_label(enquo(y_var))
-#   plot_letters <- tukey_data %>%
-#     filter(Variable == y_var_str)
-# 
-#   y_max <- max(data[[y_var_str]], na.rm = TRUE) * 1.1
-#   
-#   p <- ggplot(data = data, mapping = aes(x = Type, y = {{ y_var }})) +
-#     geom_boxplot(aes(fill = Type)) +
-#     geom_point() +
-#     scale_fill_manual(values = c("thistle", "olivedrab3", "lemonchiffon")) +
-#     labs(x = "Species Type", y = y_label) +
-#     theme_tufte(18) +
-#     theme(legend.position = "none",  panel.border = element_rect(color = "black", fill = NA, linewidth = 1)) +
-#     scale_x_discrete(labels = c(
-#       "Invasive" = "I",
-#       "Native" = "N",
-#       "Non-invasive exotic" = "NIE"
-#     ))
-# 
-#   if (nrow(plot_letters) > 0) {
-#     if (all(is.na(plot_letters$Groups))) {
-#       plot_letters$Groups <- "ns"
-#     }
-# 
-#     if (!all(is.na(plot_letters$Type))) {
-#       p <- p + geom_text(
-#         data = plot_letters,
-#         aes(x = Type, y = y_max, label = Groups),
-#         vjust = 1.5, 
-#         size = 7, 
-#         color = "black"
-#       )
-#     }
-#   }
-# 
-#   if (!is.null(plot_label)) {
-#     p <- p + 
-#       annotate("text", x = 0.5, y = Inf, label = plot_label, 
-#                hjust = 0, vjust = 2, size = 8, fontface = "bold")
-#   }
-#   
-#   # Save individual plot if file_suffix is provided
-#   if (!is.null(file_suffix)) {
-#     ggsave(paste0("isha/Plots/", file_suffix, ".png"), 
-#            plot = p, width = 7, height = 8)
-#   }
-#   
-#   return(p)
-# }
-# 
-# 
-# process_columns <- function(data, columns_to_plot, tukey_data) {
-#   for (col_spec in columns_to_plot) {
-#     col_name <- col_spec$column
-#     y_label <- col_spec$label
-#     file_suffix <- col_spec$file_suffix
-#     
-#     create_boxplot(
-#       data = data, 
-#       y_var = !!sym(col_name), 
-#       y_label = y_label, 
-#       file_suffix = file_suffix,
-#       tukey_data = tukey_data
-#     )
-#   }
-# }
-# 
-# create_four_plot_grid <- function(master_whole, master_leaf, whole_tukey, leaf_tukey) {
-#   plot1 <- create_boxplot(
-#     data = master_whole,
-#     y_var = `SLA (mm2/mg)`,
-#     y_label = "Specific Leaf Area (mm²/mg)",
-#     file_suffix = NULL,
-#     tukey_data = whole_tukey,
-#     plot_label = "A"
-#   )
-#   
-#   plot2 <- create_boxplot(
-#     data = master_whole,
-#     y_var = Tannins,
-#     y_label = "[Relative Tannins]",
-#     file_suffix = NULL,
-#     tukey_data = whole_tukey,
-#     plot_label = "B"
-#   )
-#   
-#   plot3 <- create_boxplot(
-#     data = master_leaf,
-#     y_var = `Toughness (N)`,
-#     y_label = "Leaf Toughness (N)",
-#     file_suffix = NULL,
-#     tukey_data = leaf_tukey,
-#     plot_label = "C"
-#   )
-#   
-#   plot4 <- create_boxplot(
-#     data = master_leaf,
-#     y_var = `Thickness (mm)`,
-#     y_label = "Leaf Thickness (mm)",
-#     file_suffix = NULL,
-#     tukey_data = leaf_tukey,
-#     plot_label = "D"
-#   )
-# 
-#   combined_plot <- (plot1 + plot2) / (plot3 + plot4) +
-#     plot_layout(guides = 'collect') &
-#     theme(legend.position = 'none')
-#   
-#   ggsave("isha/Plots/four_trait_grid.png", 
-#          plot = combined_plot, 
-#          width = 16,
-#          height = 16,
-#          dpi = 300)
-#   
-#   ggsave("isha/Plots/four_trait_grid.pdf", 
-#          plot = combined_plot, 
-#          width = 16,
-#          height = 16)
-#   
-#   return(combined_plot)
-# }
-# 
-# # Individual plots ----
-# 
-# master_whole <- read_csv("isha/Harvard Master.csv")
-# master_leaf <- read_csv("isha/Harvard MasterLeaf.csv")
-# 
-# columns_to_plot <- list(
-#   list(column = "Plant height (ft)", label = "Plant Height (ft)", file_suffix = "PlantHeight"),
-#   list(column = "Nitrogen content", label = "Nitrogen Content", file_suffix = "NitrogenContent"),
-#   list(column = "C:N ratio", label = "C:N Ratio", file_suffix = "CNRatio"),
-#   list(column = "Flavonoids", label = "Flavonoids", file_suffix = "Flavonoids"),
-#   list(column = "Phenolics", label = "Phenolics", file_suffix = "Phenolics"),
-#   list(column = "Terpenoids", label = "Terpenoids", file_suffix = "Terpenoids"),
-#   list(column = "Tannins", label = "Tannins", file_suffix = "Tannins"),
-#   list(column = "Average water content (%)", label = "Average Water Content (%)", file_suffix = "WaterContent"),
-#   list(column = "SLA (mm2/mg)", label = "Specific Leaf Area (mm2/mg)", file_suffix = "SpecificLeafArea")
-# )
-# 
-# columns_to_plot_leaf <- list(
-#   list(column = "Toughness (N)", label = "Toughness (N)", file_suffix = "Toughness"),
-#   list(column = "Thickness (mm)", label = "Thickness (mm)", file_suffix = "Thickness")
-# )
-# 
-# process_columns(master_whole, columns_to_plot, whole_tukey)
-# process_columns(master_leaf, columns_to_plot_leaf, leaf_tukey)
-# 
-# 
-# grid_plot <- create_four_plot_grid(master_whole, master_leaf, whole_tukey, leaf_tukey)
-# 
-# print(grid_plot)
 
 
 
